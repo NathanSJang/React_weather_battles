aa57f35dd5b05f465dbd1a846c7e8eba1d7197d2
Jang <sbtan1989@gmail.com>
2021-01-19 17:01:10 -0800

    part 1

---
+++ controllers/scores.js
@@ -0,0 +1,35 @@
+const Score = require('../models/score');
+
+module.exports = {
+  index,
+  create,
+  delete: deleteOne,
+};
+
+async function index(req, res) {
+  try {
+    const scores = await Score.find({}).sort({score: -1});
+    res.json({scores});
+  } catch (error) {
+    res.json({error});
+  }
+};
+
+async function create(req, res) {
+  try {
+    const scores = await Score.create(req.body);
+    res.redirect('/api/scores')
+  } catch (error) {
+    res.json({error});
+  }
+};
+
+async function deleteOne(req, res) {
+  try {
+    const scores = await Score.findByIdAndDelete(req.params.id);
+    res.redirect('/api/scores')
+  } catch (error) {
+    res.json({error});
+  }
+};
+
